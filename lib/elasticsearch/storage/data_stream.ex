8e8141ca287d8b3b946f473fa1d9594dae51ff8b
Daniel Berkompas <himself@danielberkompas.com>
2018-03-19 14:02:59 -0700

    [#10] Prevent ArgumentError

--- lib/elasticsearch/storage/data_stream.ex
+++ lib/elasticsearch/storage/data_stream.ex
@@ -14,7 +14,7 @@ defmodule Elasticsearch.DataStream do
   ## Configuration
 
   Your configured `:store` module must handle the given data source.
-  The stream will be paginated based on the `:bulk_page_size` in the 
+  The stream will be paginated based on the `:bulk_page_size` in the
   configuration.
 
       config :elasticsearch,
@@ -25,7 +25,7 @@ defmodule Elasticsearch.DataStream do
       iex> stream = DataStream.stream(MyApp.Schema, Elasticsearch.Test.Store)
       ...> is_function(stream)
       true
-      
+
   """
   @spec stream(source, Elasticsearch.Store.t()) :: Stream.t()
   def stream(source, store) do
@@ -34,38 +34,43 @@ defmodule Elasticsearch.DataStream do
 
   # Store state in the following format:
   #
-  # {items, offset, limit}
+  # {items, prev_page_count, offset, limit}
   defp init do
-    {[], 0, Config.all()[:bulk_page_size]}
-  end
-
-  # If no items, load another page of items
-  defp next({[], offset, limit}, source, store) do
-    load_page(source, store, offset, limit)
-  end
-
-  # If there are items, return the next item, and set the new state equal to
-  # {tail, offset, limit}
-  defp next({[h | t], offset, limit}, _source, _store) do
-    {[h], {t, offset, limit}}
-  end
-
-  # Fetch a new page of items
-  defp load_page(source, store, offset, limit) do
     page_size = Config.all()[:bulk_page_size]
+    {[], page_size, 0, page_size}
+  end
 
+  # If no items, and the previous page was equal to the expected size,
+  # then load another page of items.
+  #
+  # There might be another page of data to fetch.
+  defp next({[], prev_page_size, offset, limit} = state, source, store)
+       when prev_page_size == limit do
     case store.load(source, offset, limit) do
       # If the load returns no more items (i.e., we've iterated through them
       # all) then halt the stream and leave offset and limit unchanged.
       [] ->
-        {:halt, {[], offset, limit}}
+        {:halt, state}
 
       # If the load returns items, then return the first item, and put the
-      # tail into the state. Also, increment offset and limit by the
-      # configured `:bulk_page_size`.
-      [h | t] ->
-        {[h], {t, offset + page_size, limit}}
+      # tail into the state. Also, increment offset by the configured
+      # `:bulk_page_size`.
+      [h | t] = items ->
+        {[h], {t, length(items), offset + limit, limit}}
     end
+  end
+
+  # If there are no remaining items, and the previous page size was smaller
+  # than the expected page size, we know we are at the end, so we halt.
+  defp next({[], prev_page_size, _offset, limit} = state, _source, _store)
+       when prev_page_size < limit do
+    {:halt, state}
+  end
+
+  # If there are items, return the next item, and set the new state equal to
+  # {tail, offset, limit}
+  defp next({[h | t], prev_page_size, offset, limit}, _source, _store) do
+    {[h], {t, prev_page_size, offset, limit}}
   end
 
   # We don't need to do anything to clean up this Stream
--- test/elasticsearch/storage/data_stream_test.exs
+++ test/elasticsearch/storage/data_stream_test.exs
@@ -4,4 +4,36 @@ defmodule Elasticsearch.DataStreamTest do
   alias Elasticsearch.DataStream
 
   doctest Elasticsearch.DataStream
+
+  defmodule TestStore do
+    def load(_thing, offset, _limit) when offset == 0 do
+      send(self(), :loaded_once)
+
+      [
+        %{one: 1},
+        %{two: 2},
+        %{three: 3}
+      ]
+    end
+
+    def load(_thing, _offset, _limit) do
+      send(self(), :loaded_twice)
+      []
+    end
+  end
+
+  # See https://github.com/infinitered/elasticsearch-elixir/issues/10
+  @tag :regression
+  test "handles store values" do
+    stream = DataStream.stream(:thing, TestStore)
+
+    assert Enum.to_list(stream) == [
+             %{one: 1},
+             %{two: 2},
+             %{three: 3}
+           ]
+
+    assert_received :loaded_once
+    refute_received :loaded_twice
+  end
 end
