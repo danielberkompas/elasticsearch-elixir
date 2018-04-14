1f8b7096dab1638ac1391355187f53e1e72f3398
Daniel Berkompas <himself@danielberkompas.com>
2018-04-14 10:08:01 -0700

    Make executable test compatible with Travis

--- test/elasticsearch/executable_test.exs
+++ test/elasticsearch/executable_test.exs
@@ -5,17 +5,21 @@ defmodule Elasticsearch.ExecutableTest do
 
   import ExUnit.CaptureIO
 
+  defp elasticsearch do
+    case System.cmd("which", ["elasticsearch"]) do
+      {"", _} ->
+        "vendor/elasticsearch/bin/elasticsearch"
+
+      {path, 0} ->
+        path
+    end
+  end
+
   describe ".start_link/3" do
     test "starts the executable if it isn't running" do
       output =
         capture_io(fn ->
-          assert {:ok, pid} =
-                   Executable.start_link(
-                     "Elasticsearch",
-                     "vendor/elasticsearch/bin/elasticsearch",
-                     9201
-                   )
-
+          assert {:ok, pid} = Executable.start_link("Elasticsearch", elasticsearch(), 9201)
           GenServer.stop(pid)
         end)
 
@@ -26,13 +30,7 @@ defmodule Elasticsearch.ExecutableTest do
     test "does nothing if executable is already running" do
       output =
         capture_io(fn ->
-          assert {:ok, pid} =
-                   Executable.start_link(
-                     "Elasticsearch",
-                     "vendor/elasticsearch/bin/elasticsearch",
-                     9200
-                   )
-
+          assert {:ok, pid} = Executable.start_link("Elasticsearch", elasticsearch(), 9200)
           GenServer.stop(pid)
         end)
 
