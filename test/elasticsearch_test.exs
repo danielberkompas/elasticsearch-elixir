0d6c6db04e9a401378d45cd827736f5d1a6b09dd
Daniel Berkompas <himself@danielberkompas.com>
2019-01-19 07:55:55 -0800

    [#60] Add Elasticsearch.post_document/3
    You must post documents to the API in order for Elasticsearch's
    automatic ID generation to work.
    
    See [the Elasticsearch
    documentation](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html#_automatic_id_generation) for more details.
    
    Closes #60.

--- test/elasticsearch_test.exs
+++ test/elasticsearch_test.exs
@@ -21,6 +21,8 @@ defmodule ElasticsearchTest do
 
   describe ".put_document/3" do
     test "routing meta-field is included if specified in Document" do
+      Elasticsearch.delete(Cluster, "/posts-routing")
+
       assert :ok =
                Elasticsearch.Index.create_from_file(
                  Cluster,
@@ -43,6 +45,44 @@ defmodule ElasticsearchTest do
                  %Comment{id: 2, body: "Example Comment", author: "Jane Smith", post_id: 1},
                  "posts-routing"
                )
+
+      Elasticsearch.delete(Cluster, "/posts-routing")
+    end
+  end
+
+  describe ".post_document/3" do
+    # GitHub: https://github.com/infinitered/elasticsearch-elixir/issues/60
+    @tag timeout: :infinity
+    @tag :regression
+    test "does not post ID when ID is nil" do
+      Elasticsearch.delete(Cluster, "/posts-id")
+
+      assert :ok =
+               Elasticsearch.Index.create_from_file(
+                 Cluster,
+                 "posts-id",
+                 "test/support/settings/posts.json"
+               )
+
+      assert {:ok, _} =
+               Elasticsearch.post_document(
+                 Cluster,
+                 %Post{title: "Example Post", author: "John Smith"},
+                 "posts-id"
+               )
+
+      Elasticsearch.Index.refresh!(Cluster, "posts-id")
+      {:ok, index} = Elasticsearch.get(Cluster, "/posts-id/_search")
+
+      assert get_in(index, ["hits", "hits", Access.at(0), "_id"]) != nil
+
+      assert get_in(index, ["hits", "hits", Access.at(0), "_source"]) == %{
+               "author" => "John Smith",
+               "doctype" => %{"name" => "post"},
+               "title" => "Example Post"
+             }
+
+      Elasticsearch.delete(Cluster, "/posts-id")
     end
   end
 end
