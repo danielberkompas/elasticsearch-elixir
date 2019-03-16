80f84f22daefc8304c0941d4516ec1a79b7f3fd3
Marcelo Lebre <marcelo.lebre@gmail.com>
2019-03-16 22:17:52 +0000

    Add pattern matching to function to build signed request based on default_headers

--- lib/elasticsearch/api/aws.ex
+++ lib/elasticsearch/api/aws.ex
@@ -104,7 +104,13 @@ defmodule Elasticsearch.API.AWS do
     ]
   end
 
-  def build_signed_request(url, options, default_headers \\ %{}) do
+  def build_signed_request(url, options, default_headers) when is_nil(default_headers) do
+    {:ok, signed_data, _} = Sigaws.sign_req(url, options)
+
+    Map.merge(%{"Content-Type": "application/json"}, signed_data)
+  end
+
+  def build_signed_request(url, options, default_headers) do
     {:ok, signed_data, _} = Sigaws.sign_req(url, options)
 
     default_headers
