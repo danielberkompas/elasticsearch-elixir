8084ecf6acf1179f405458a5e2c163e4ff3b303c
Daniel Berkompas <himself@danielberkompas.com>
2021-01-24 01:36:09 +0000

    :white_check_mark: Add test coverage for #81
    See #81

--- test/elasticsearch/api/http_test.exs
+++ test/elasticsearch/api/http_test.exs
@@ -27,5 +27,18 @@ defmodule Elasticsearch.API.HTTPTest do
                  []
                )
     end
+
+    # See https://github.com/danielberkompas/elasticsearch-elixir/issues/81
+    @tag :regression
+    test "handles timeouts" do
+      assert {:error, %HTTPoison.Error{reason: :timeout}} =
+               HTTP.request(
+                 %{},
+                 :get,
+                 "http://#{System.get_env("ELASTICSEARCH_HOST", "localhost")}:9200/_cat/health",
+                 "",
+                 recv_timeout: 0
+               )
+    end
   end
 end
