955dda988e0ff3b41ba6d7e6807cfaf3912db54c
Christian Koch <54853932+ckoch-cars@users.noreply.github.com>
2023-07-12 13:15:13 -0500

    support bulk delete requests
    When the encoded struct 'document' is appended to a bulk request, it causes the bulk request to fail.

--- test/elasticsearch/indexing/bulk_test.exs
+++ test/elasticsearch/indexing/bulk_test.exs
@@ -87,4 +87,11 @@ defmodule Elasticsearch.Index.BulkTest do
                "\"_routing\":\"123\""
     end
   end
+
+  describe ".encode!/4" do
+    test "'delete' omits the struct from the body" do
+      assert "{\"delete\":{\"_routing\":\"123\",\"_index\":\"my-index\",\"_id\":\"my-id\"}}\n" ==
+               Bulk.encode!(Cluster, %Comment{id: "my-id", post_id: "123"}, "my-index", "delete")
+    end
+  end
 end
